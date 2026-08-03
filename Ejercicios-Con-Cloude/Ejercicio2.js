8d70f758581dac02259335844a581d97388b2e80
Sebastian_Lamprea <juansebastianlamprearojas@gmail>
2026-08-02 22:55:50 -0500

    Realizacion de Ejercicios nivel 1 y nivel 2

--- Ejercicios-Con-Cloude/Ejercicio2.js
+++ Ejercicios-Con-Cloude/Ejercicio2.js
@@ -173,10 +173,140 @@
 
 
 //EJERCICIO 3/ NIVEL 1
-let cajaNormal = document.getElementById("cajaOriginal");
-let cajaNueva = document.getElementById("cajaNuevoPlaceholder");
-let boton = document.getElementById("botonCambiarPlaceholder");
+// let cajaNormal = document.getElementById("cajaOriginal");
+// let cajaNueva = document.getElementById("cajaNuevoPlaceholder");
+// let boton = document.getElementById("botonCambiarPlaceholder");
 
-boton.addEventListener("click", () => {
+// boton.addEventListener("click", () => {
+//     let nuevoPlaceholder = cajaNueva.placeholder;
 
-});
+// });
+
+
+//EJERCICIO 4/ NIVEL 2
+// let botonSumar = document.getElementById("botonSumar");
+// let botonRestar = document.getElementById("botonRestar");
+// let contar = document.getElementById("contadorAjustable");
+// let contador = 0;
+
+// botonSumar.addEventListener("click", () => {
+//     contador++;
+//     contar.innerHTML = contador;
+// });
+// botonRestar.addEventListener("click", () => {
+//     contador--;
+//     contar.innerHTML = contador;
+// });
+
+
+//EJERCICIO 5/ NIVEL 2
+// let caja = document.getElementById("cajaCodigo");
+// let boton = document.getElementById("botonDesbloquear");
+// let mensaje = document.getElementById("mensajeCodigo");
+
+// boton.addEventListener("click", () => {
+//     let codigo = caja.value;
+    
+//     if (codigo === "1234") {
+//         mensaje.textContent = "¡Codigo correcto! Acceso concedido.";
+//         mensaje.style.color = "green";
+
+//         boton.disabled = true;
+//         caja.disabled = true;
+//     } else {
+//         mensaje.textContent = "Codigo incorrecto. Intenta nuevamente.";
+//         mensaje.style.color = "red";
+
+//         caja.value = "";
+//         caja.focus();
+//     }
+// });
+
+
+//EJERCICIO 6/ NIVEL 1
+// let cajaOriginal = document.getElementById("cajaOriginalTexto");
+// let cajaBuscar = document.getElementById("cajaBuscar");
+// let cajaRemplazo = document.getElementById("cajaReemplazo");
+// let botonRemplazar = document.getElementById("botonReemplazar");
+// let resultado = document.getElementById("resultadoReemplazo");
+
+// botonRemplazar.addEventListener("click", () => {
+//     let textoOriginal = cajaOriginal.value;
+//     let textoBuscar = cajaBuscar.value;
+//     let textoRemplazo = cajaRemplazo.value;
+
+//     let nuevoTexto = textoOriginal.replaceAll(textoBuscar, textoRemplazo);
+//     resultado.innerHTML = "";
+//     resultado.innerHTML += nuevoTexto;
+// });
+
+
+//EJERCICIO 7/ NIVEL 2
+// let botonLimite = document.getElementById("botonClickLimite");
+// let contadorLimite = document.getElementById("contadorLimite");
+// let contador = 0;
+
+// botonLimite.addEventListener("click", () => {
+//     if (contador < 5) {
+//         contador++;
+//         contadorLimite.innerHTML = contador;
+//     } else {
+//         alert("¡Has alcanzado el límite de 5 clics!");
+//         contadorLimite.innerHTML = "Límite alcanzado";
+//     }
+// });
+
+
+//EJERCICIO 8/ NIVEL 2
+// let cajaContraseña = document.getElementById("cajaContrasena");
+// let resultadoContraseña = document.getElementById("resultadoContrasena");
+// let contador = 0;
+
+// cajaContraseña.addEventListener("keyup", (e) => {
+//     let contraseña = e.target.value;
+//     resultadoContraseña.textContent = "";
+
+//     if (contraseña.length < 6) {
+//         resultadoContraseña.textContent = "Contraseña débil";
+//         resultadoContraseña.style.color = "red";
+//     } else {
+//         resultadoContraseña.textContent = "Contraseña fuerte";
+//         resultadoContraseña.style.color = "green";
+//     }
+// });
+
+
+//EJERCICIO 9/ NIVEL 2
+// let cajaClave1 = document.getElementById("cajaClave1");
+// let cajaClave2 = document.getElementById("cajaClave2");
+// let mensajeCoincide = document.getElementById("mensajeCoincide");
+
+// cajaClave2.addEventListener("keyup", (e) => {
+//     let clave1 = cajaClave1.value;
+//     let clave2 = e.target.value;
+
+//     if (clave1 === clave2) {
+//         mensajeCoincide.textContent = "¡Las contraseñas coinciden!";
+//         mensajeCoincide.style.color = "green";
+//     } else {
+//         mensajeCoincide.textContent = "Las contraseñas no coinciden.";
+//         mensajeCoincide.style.color = "red";
+//     }
+// });
+
+
+//EJERCICIO 10/ NIVEL 2
+// let botonClaro = document.getElementById("botonClaro");
+// let botonOscuro = document.getElementById("botonOscuro");
+// let mensajeTema = document.getElementById("mensajeTema");
+
+// botonClaro.addEventListener("click", () => {
+//     document.body.style.backgroundColor = "white";
+//     document.body.style.color = "black";
+//     mensajeTema.textContent = "Modo claro activado";
+// });
+// botonOscuro.addEventListener("click", () => {
+//     document.body.style.backgroundColor = "black";
+//     document.body.style.color = "white";
+//     mensajeTema.textContent = "Modo oscuro activado";
+// });
